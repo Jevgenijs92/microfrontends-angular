76a602ece7aae19838a4e2e8e664f467118fbe58
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2023-10-06 09:24:36 +0300

    change endpoint name

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,6 +1,6 @@
 import { initFederation } from '@angular-architects/module-federation';
 
-initFederation('http://localhost:3000/manifest') // http://localhost:3000/manifest | assets/mf.manifest.json
+initFederation('http://localhost:3000/remote-routes') // http://localhost:3000/remote-routes | assets/mf.manifest.json
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
